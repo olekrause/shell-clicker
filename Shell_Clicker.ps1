9dadf6ea65577107eec4363706d65645a03410d3
olekrause <aiciquiu300@gmail.com>
2023-03-29 15:17:17 +0200

    Update Shell_Clicker.ps1

--- Shell_Clicker.ps1
+++ Shell_Clicker.ps1
@@ -40,20 +40,17 @@ function SpS_calc {
 function dis_fun {
 	SpS_calc
 	price_fun
+	$dis_shells = [string]::Format('{0:N0}', $array.shells.shell_amount)
+	$dis_SpS = [string]::Format('{0:N0}', $array.shells.SpS)
 	Clear-Host
-	Write-Host "Shells:"$array.shells.shell_amount
-	Write-Host "SpS (Shells pro Sekunde):"$array.shells.SpS
+	Write-Host "Shells:"$dis_shells
+	Write-Host "SpS (Shells pro Sekunde):"$dis_SpS
 	Write-Host ""
 	for ($i = 0; $i -lt $array['buildings'].Count; $i++) {
 		$dis_SpS = [string]::Format('{0:N0}', $array.buildings[$i].SpS)
-		$dis_Amount = [string]::Format('{0:N0}', $array.buildings[$i].Amount)
+		$dis_amount = [string]::Format('{0:N0}', $array.buildings[$i].Amount)
 		$dis_price = [string]::Format('{0:N0}', $array.buildings[$i].current_price)
-		if ($i -lt 8) {
-			Write-Host $array.buildings[$i].Name "("$dis_SpS "SpS) - Menge:		" $dis_Amount "	Preis: "$dis_price
-		}
-		else {
-			Write-Host $array.buildings[$i].Name "("$dis_SpS "SpS) - Menge:	" $dis_Amount "	Preis: "$dis_price
-		}
+			Write-Host $array.buildings[$i].Name "($dis_SpS SpS)	-	Preis:	"$dis_price"`nMenge:	"$dis_amount "`n"
 	}
 }
 
